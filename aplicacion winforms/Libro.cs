433d439cd6f9345abc2ce5f1664d051c27ff2e83
Darivh03 <driverah2@miumg.edu.gt>
2025-03-17 16:08:25 -0600

    Agregar archivos de proyecto.

---
+++ aplicacion winforms/Libro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace aplicacion_winforms
+{
+    class Libro
+    {
+        public class BibliotecaContext : DbContext
+        {
+            public DbSet<Libro> Libros { get; set; }
+            public DbSet<Usuario> Usuarios { get; set; }
+            public DbSet<Prestamo> Prestamos { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-7ALR3JB;Database=BibliotecaDB;Trusted_Connection=True;");
+            }
+        }
+    }
+}
