4b959297d42d11451ec13800e0d13a04134581b9
Darivh03 <driverah2@miumg.edu.gt>
2025-03-17 16:57:24 -0600

    creacion de 2 clases, una para la conexion con la base de datos y la otra para definir la tabla libros

---
+++ aplicacion winforms/BibliotecaContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace aplicacion_winforms
+{
+    public class BibliotecaContext
+    {
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Prestamo> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-7ALR3JB;Database=BibliotecaDB;User Id=sa;Password=Rivera.03;");
+        }
+    }
+}
+
+    
+
--- aplicacion winforms/Libro.cs
+++ aplicacion winforms/Libro.cs
@@ -4,21 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace aplicacion_winforms
 {
-    class Libro
-    {
-        public class BibliotecaContext : DbContext
+
+        public class Libro
         {
-            public DbSet<Libro> Libros { get; set; }
-            public DbSet<Usuario> Usuarios { get; set; }
-            public DbSet<Prestamo> Prestamos { get; set; }
+            [Key]  // Define la clave primaria
+            public int Id_Libro { get; set; }
+
+            [Required] // Campo obligatorio
+            public string Titulo { get; set; }
+
+            [Required]
+            public string Autor { get; set; }
+
+            public string Categoria { get; set; }
+
+            public bool Disponible { get; set; }
 
             protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
-                optionsBuilder.UseSqlServer("Server=DESKTOP-7ALR3JB;Database=BibliotecaDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-7ALR3JB;Database=BibliotecaDB;User Id=sa;Password=Rivera.03;");
+
             }
         }
-    }
+    
 }
